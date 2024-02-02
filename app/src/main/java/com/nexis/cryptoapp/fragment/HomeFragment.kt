6c6de8c970eca70440777e49c1ae9d5a4309e4f8
AkramAkbarov <153746309+AkramAkbarov@users.noreply.github.com>
2024-02-02 11:31:27 +0400

    cr

--- app/src/main/java/com/nexis/cryptoapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/nexis/cryptoapp/fragment/HomeFragment.kt
@@ -9,11 +9,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import com.nexis.cryptoapp.R
+import com.nexis.cryptoapp.adapter.TopMarketAdapter
 import com.nexis.cryptoapp.apis.ApiInterface
 import com.nexis.cryptoapp.apis.ApiUtilities
 import com.nexis.cryptoapp.databinding.FragmentHomeBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class HomeFragment : Fragment() {
     private lateinit var binding :FragmentHomeBinding
@@ -25,14 +27,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentHomeBinding.inflate(layoutInflater)
+        getTopCurrencyList()
         return binding.root
 
-        getTopCurrencyList()
+
     }
 
     private fun getTopCurrencyList() {
         lifecycleScope.launch (Dispatchers.IO){
             val res = ApiUtilities.getInstance().create(ApiInterface::class.java).getMarketData()
+
+            withContext(Dispatchers.Main){
+                binding.topCurrencyRecyclerView.adapter=TopMarketAdapter(requireContext(),res.body()!!.data.cryptoCurrencyList)
+            }
+
 
             Log.d("SHUBH","getTopCurrencyList: ${res.body()!!.data.cryptoCurrencyList}")
         }
