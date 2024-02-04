424d320c5776f0a7b45807a336d4aa7c910c65ee
AkramAkbarov <153746309+AkramAkbarov@users.noreply.github.com>
2024-02-04 11:31:39 +0400

    cr

--- app/src/main/java/com/nexis/cryptoapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/nexis/cryptoapp/fragment/HomeFragment.kt
@@ -8,7 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
+import androidx.transition.Visibility
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.GONE
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import androidx.viewpager2.widget.ViewPager2.VISIBLE
+import com.google.android.material.tabs.TabLayoutMediator
 import com.nexis.cryptoapp.R
+import com.nexis.cryptoapp.adapter.TopLossGainPagerAdapter
 import com.nexis.cryptoapp.adapter.TopMarketAdapter
 import com.nexis.cryptoapp.apis.ApiInterface
 import com.nexis.cryptoapp.apis.ApiUtilities
@@ -28,9 +35,39 @@ class HomeFragment : Fragment() {
     ): View? {
         binding= FragmentHomeBinding.inflate(layoutInflater)
         getTopCurrencyList()
+        setTabLayout()
         return binding.root
 
 
+    }
+
+    private fun setTabLayout() {
+        val adapter =TopLossGainPagerAdapter(this)
+        binding.contentViewPager.adapter=adapter
+        binding.contentViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == 0) {
+                    binding.topGainIndicator.visibility = View.VISIBLE
+                    binding.topLoseIndicator.visibility = View.GONE
+                } else {
+                    binding.topGainIndicator.visibility = View.GONE
+                    binding.topLoseIndicator.visibility = View.VISIBLE
+                }
+            }
+
+        })
+
+        TabLayoutMediator(binding.tabLayout,binding.contentViewPager){
+            tab,positon ->
+            var title = if (positon==0){
+                "Top Gainers"
+            }else{
+                "Top Losers"
+            }
+            tab.text =title
+        }.attach()
     }
 
     private fun getTopCurrencyList() {
